811bcbd562dc096961c2f53e1b6b7f495951b4fe
Rupal144aggarwal <56926088+Rupal144aggarwal@users.noreply.github.com>
2019-10-23 19:56:17 +0530

    code optimality changed
    minor changes to run code smoothly

--- scatterplot.py
+++ scatterplot.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
-x=[1,2,3,4,5,6,7,8,9]
-y=[2,4,6,3,1,7,9,5,5]
-bins=10
-plt.scatter(x,y,bins,color='red',label='star',marker='*')
+x=[1,2,3,4,5,6,7,8,9,10,11]
+y=[2,4,6,3,1,7,9,5,5,7,4]
+bins=11
+plt.scatter(x,y,bins,color='blue',label='star',marker='*')
 plt.legend()
 plt.show()
 
