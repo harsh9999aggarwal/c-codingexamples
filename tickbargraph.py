66ef857e2e54fd62f04a8e9abf850b0288faeb9a
priteshsingh-vats <42402753+priteshsingh-vats@users.noreply.github.com>
2019-10-25 00:31:39 +0530

    Update tickbargraph.py
    made some important changes

--- tickbargraph.py
+++ tickbargraph.py
@@ -10,3 +10,21 @@ y=[2,4,5,7,11,6,9,12,2,4]
 tick_label=['one','two','three','our','five','six','seven','two','three','our']
 plt.bar(x,y,tick_label=tick_label,width=0.2,color=['red','green'])
 plt.show()
+import matplotlib.pyplot as plt
+img1 = image.load_img(img_path,target_size=(100,100))
+
+x = image.img_to_array(img1)/255.0
+plt.axis("off")
+plt.imshow(x)
+plt.show()
+print(x.shape)
+x =x.reshape((1,100,100,3))
+datagen = image.ImageDataGenerator(
+                                      rotation_range=30,
+                                      width_shift_range=0.2,
+                                      height_shift_range=0.2,
+                                      shear_range =0.2,
+                                       zoom_range =0.2,
+                                     horizontal_flip = True,
+                                       fill_mode ="nearest"
+)
